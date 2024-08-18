c56fe16955170ac7c16eccfc7bdebaaa674d2e76
xieqifeng <xieqifeng@xiaomi.com>
2024-08-18 22:07:10 +0800

    更新到生产的配置

--- app.py
+++ app.py
@@ -38,25 +38,91 @@ if not app.debug:
 
 from gevent import pywsgi
 
+CLIENT_SN = "d8132af12248"
 # MQTT服务器配置
 MQTT_BROKER = '47.121.203.152'  # MQTT服务器地址
 MQTT_PORT = 1883  # MQTT服务器端口
-MQTT_TOPIC = '/sys/folotoy/d8132af12248/thing/command/call'  # 要发送消息的topic
+COMMAND_CALL_TOPIC = f'/sys/folotoy/{CLIENT_SN}/thing/command/call'  # 要发送消息的topic
+EVENT_POST_TOPIC = f'/sys/folotoy/{CLIENT_SN}/thing/event/post'
+AUDIO_PREFIX = "http://47.121.203.152:8082/"
 ALLOWED_EXTENSIONS = set(['wav', 'txt'])
 cosy_voice_url = 'http://43.240.0.168:6006/inference/stream'
 prompt_text = "这里住着一只聪明的小动物，它的名字叫做小悟星。"
+# 创建MQTT客户端实例
+client = mqtt.Client()
 
 
 # MQTT回调函数
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     # 订阅topic
-    client.subscribe(MQTT_TOPIC)
+    client.subscribe(COMMAND_CALL_TOPIC)
+    client.subscribe(EVENT_POST_TOPIC)
 
 
 # MQTT消息接收函数
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    with app.app_context():
+        if msg.topic == COMMAND_CALL_TOPIC:
+            pass
+        elif msg.topic == EVENT_POST_TOPIC:
+            # 处理EVENT POST 主题的信息
+            app.logger.info(msg.payload.decode())
+            message_data = json.loads(msg.payload.decode())
+            if message_data['identifier'] == 'recording_transcribed':
+                recording_text = message_data["inputParams"]["recordingText"].encode('utf-8').decode()
+                app.logger.info(recording_text)
+                new_dialogue = Dialogue(user_id=1, role="child", content=recording_text)
+                # 这里是来自客户的语音输入，应该是回答问题的部分。
+                if f"{CLIENT_SN}_session" in thread_results:
+                    session_info = thread_results[f"{CLIENT_SN}_session"]
+                else:
+                    session_info = {
+                        "sessionId": create_session(),
+                        "story_id": 1,
+                        "voice_id": 1
+                    }
+                session_id = session_info.session_id
+                story_id = session_info.story_id
+                voice_id = session_info.voice_id
+                chat_data = create_chat(session_id, recording_text)
+                state = retrieve_chat(session_id, chat_data["id"])
+                while state != "completed":
+                    print(f"{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())} - chat_state: {state}")
+                    state = retrieve_chat(session_id, chat_data["id"])
+                    time.sleep(0.5)
+                # 获取对话列表
+                answer = chat_list(session_id, chat_data["id"])
+                for data in answer:
+                    if data["type"] == "answer":
+                        coze_response = data["content"]
+                        if coze_response.startswith("【主观评语】"):
+                            evaluate = data["content"].split("\n")[0].split("：")[-1]
+
+                        app.logger.info(f"是bot的回答，转为语音:{evaluate}")
+                        evaluate_audio = get_audio_stream(story_id, voice_id, evaluate)
+                        msg_1 = {"msgId": 1, "identifier": "iwantplay",
+                                 "inputParams": {"role": 2, "url": f"{AUDIO_PREFIX} + {os.path.basename(evaluate_audio)}"}}
+                        app.logger.info(f"Audio file saved to {evaluate_audio}")
+                        # 向MQTT服务器发送消息
+                        client.publish(COMMAND_CALL_TOPIC, payload=json.dumps(msg_1))
+            elif message_data['identifier'] == 'voice_generated':
+                if "voiceText" in message_data["inputParams"]:
+                    voice_text = message_data["inputParams"]["voiceText"].encode('utf-8').decode()
+                    app.logger.info(voice_text)
+                    new_dialogue = Dialogue(user_id=1, role="xiaowu", content=voice_text)
+            if new_dialogue:
+                # 添加到会话
+                db.session.add(new_dialogue)
+                try:
+                    # 提交会话
+                    db.session.commit()
+                    app.logger.info(f"对话提交成功！")
+                except Exception as e:
+                    # 如果发生错误，回滚会话
+                    db.session.rollback()
+                    app.logger.error(e)
+                    app.logger.info(f"对话提交数据库失败")
 
 
 def save_audio_stream(story_id, voice_id, query, prompt_speech=None):
@@ -91,8 +157,9 @@ def save_audio_stream(story_id, voice_id, query, prompt_speech=None):
         app.logger.info(f"生成语音文件{output_path}")
         # 加载 .wav 文件
         sound = AudioSegment.from_wav(output_path)
+        # TODO 修改此处保存位置为nginx的对应目录
         # 导出为 .mp3 格式
-        output_mp3_path = os.path.join(app.root_path, "tmp", "sounds", file_name + ".mp3")
+        output_mp3_path = os.path.join("/root/workspace/folotoy-server-self-hosting/audio", file_name + ".mp3")
         app.logger.info(f"转换成mp3文件: {output_mp3_path}")
         sound.export(output_mp3_path, format="mp3")
         app.logger.info(f"花费时间: {time.time() - start_time}")
@@ -101,15 +168,14 @@ def save_audio_stream(story_id, voice_id, query, prompt_speech=None):
         print("Error:", response.text)
 
 
-# 创建MQTT客户端实例
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-# 连接到MQTT服务器
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
-
-# 启动MQTT客户端
-client.loop_start()
+# 启动 MQTT 客户端
+def start_mqtt_client():
+    client.on_connect = on_connect
+    client.on_message = on_message
+    # 连接到MQTT服务器
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    # 启动MQTT客户端
+    client.loop_start()
 
 
 @app.route('/api/story/play', methods=['GET'])
@@ -122,7 +188,7 @@ def play_story():
     msg_1 = {"msgId": 1, "identifier": "iwantplay",
              "inputParams": {"role": 2, "url": "http://47.121.203.152:8082/mcc.mp3"}}
     # 向MQTT服务器发送消息
-    client.publish(MQTT_TOPIC, payload=json.dumps(msg_1))
+    client.publish(COMMAND_CALL_TOPIC, payload=json.dumps(msg_1))
     return jsonify({'status': 'success', 'message': 'Story play command sent to MQTT.'}), 200
 
 
@@ -189,13 +255,13 @@ def play_story_by_id_and_voice(story_id, voice_id):
     silence = AudioSegment.silent(duration=1500)
     # 将静音片段插入到两段音频之间
     combined_audio = audio1 + silence + audio2
-    combined_audio_path = os.path.join(app.root_path, "tmp", "sounds", f"{story_id}_{voice_id}.mp3")
+    combined_audio_path = os.path.join("/root/workspace/folotoy-server-self-hosting/audio", f"{story_id}_{voice_id}.mp3")
     combined_audio.export(combined_audio_path, format="mp3")
     msg_1 = {"msgId": 1, "identifier": "iwantplay",
-             "inputParams": {"role": 2, "url": combined_audio_path}}
+             "inputParams": {"role": 2, "url": f"{AUDIO_PREFIX}{os.path.basename(combined_audio_path)}"}}
     app.logger.info(f"Audio file saved to {combined_audio_path}")
     # 向MQTT服务器发送消息
-    client.publish(MQTT_TOPIC, payload=json.dumps(msg_1))
+    client.publish(COMMAND_CALL_TOPIC, payload=json.dumps(msg_1))
     return jsonify({'status': 'success', 'message': 'Story play command sent to MQTT.'}), 200
 
 
@@ -238,6 +304,11 @@ def get_story_question_by_id_and_voice(story_id, voice_id):
         content = story.content
         # 请求coze获取故事对应的问题
         session_id = create_session()
+        thread_results[f"{CLIENT_SN}_question_audio"] = {
+            "session_id": session_id,
+            "story_id": story_id,
+            "voice_id": voice_id,
+        }
         chat_data = create_chat(session_id, content)
         state = retrieve_chat(session_id, chat_data["id"])
         while state != "completed":
@@ -277,5 +348,9 @@ def record_voice():
 
 
 if __name__ == '__main__':
+    # 启动 MQTT 客户端线程
+    mqtt_thread = threading.Thread(target=start_mqtt_client)
+    mqtt_thread.daemon = True
+    mqtt_thread.start()
     server = pywsgi.WSGIServer(('0.0.0.0', 6000), app)
     server.serve_forever()
